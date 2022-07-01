abf366bb5e648fd0c48c4fdcbe5b2f6b0b561198
All3yp <noyallepereira@gmail.com>
2022-06-30 23:09:46 -0300

    ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nopereir <nopereir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 09:17:12 by nopereir          #+#    #+#             */
+/*   Updated: 2022/06/20 22:32:33 by nopereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*res;
+
+	if (!s)
+		return (0);
+	if (ft_strlen(s) < start)
+		len = 0;
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+	res = malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (0);
+	ft_strlcpy(res, s + start, len + 1);
+	return (res);
+}
