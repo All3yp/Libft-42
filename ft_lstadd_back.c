ceaac29cc31ca0188afcb8d549179dda90ead3c6
All3yp <noyallepereira@gmail.com>
2022-07-01 23:28:44 -0300

    ft_lstadd_back.c

---
+++ ft_lstadd_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nopereir <nopereir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 15:37:40 by nopereir          #+#    #+#             */
+/*   Updated: 2022/07/01 22:47:49 by nopereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*node;
+
+	if (!lst || !new)
+		return ;
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		node = ft_lstlast(*lst);
+		node->next = new;
+	}
+}
