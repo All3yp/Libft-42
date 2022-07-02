c262175829eab33059119ec3896780f97012bfba
All3yp <noyallepereira@gmail.com>
2022-07-01 23:29:13 -0300

    ft_lstdelone.c

---
+++ ft_lstdelone.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nopereir <nopereir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 15:41:11 by nopereir          #+#    #+#             */
+/*   Updated: 2022/07/01 15:47:32 by nopereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (del == NULL || lst == NULL)
+		return ;
+	del(lst->content);
+	free(lst);
+}
